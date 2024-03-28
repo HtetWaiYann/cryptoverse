a2d70844822c339f0ac3f0d5bfccac598ee152b0
Htet Wai Yan <hw.yan@mit.com.mm>
2024-03-28 11:22:04 +0700

    feat: global crypt stats api integration

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,6 +1,32 @@
+import { Typography, Row, Col, Statistic } from "antd"
+import millify from "millify"
+import { Link } from "react-router-dom"
+
+import { useGetCryptosQuery } from "../services/cryptoAPI"
+import { Stats } from "../types/coinType.d"
+
+
+const { Title } = Typography
+
+
 const Homepage = () => {
+
+  const { data, isFetching } = useGetCryptosQuery('');
+  const globalStats : Stats | undefined = data?.data?.stats;
+
+  if(isFetching || !globalStats) return "Loading...";
+
   return (
-    <div>Homepage</div>
+    <>
+      <Title level={2} className="heading">Global Crypto Stats</Title>
+      <Row>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value={globalStats.totalExchanges} /></Col>
+        <Col span={12}><Statistic title="Total Market Cap" value={globalStats.totalMarketCap} /></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value={globalStats.total24hVolume} /></Col>
+        <Col span={12}><Statistic title="Total Markets" value={globalStats.totalMarkets} /></Col>
+      </Row>
+    </>
   )
 }
 
---
+++ src/services/cryptoAPI.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { GetCoinsApiResponse } from "../types/coinType.d";
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Key": "55ed38359fmshcf3aa76be053ca2p139905jsn690b32b5b850",
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url: string) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query<GetCoinsApiResponse, string>({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
